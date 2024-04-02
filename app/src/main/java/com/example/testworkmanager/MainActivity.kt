251cf42a8c0ef154d3ba2b57b6b3527732e4dd45
chenzhizsqq <chenzhi@qq.com>
2024-04-02 11:05:13 +0900

    使用 WorkManager#enqueueUniquePeriodicWork()，让APP每15分钟，给手机发送一次通知 https://zhuanlan.zhihu.com/p/265051417

--- app/src/main/java/com/example/testworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/testworkmanager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testworkmanager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import com.example.testworkmanager.databinding.ActivityMainBinding
@@ -22,14 +23,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonStart.setOnClickListener {
 
+            //每十五分钟一次发信息给android机上
             val periodicWork = PeriodicWorkRequest.Builder(
                 MyWorker::class.java,
                 Duration.ofMinutes(15)
             ).build()
 
-            val operation = manager.enqueue(periodicWork)
+            //val operation = manager.enqueue(periodicWork)
 
-
+            //使用 WorkManager#enqueueUniquePeriodicWork() 将您的 WorkRequest 作为独特任务 (unique Work) 加入队列：
+            WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+                "MyUniqueWorkName",
+            ExistingPeriodicWorkPolicy.KEEP,
+                periodicWork)
         }
     }
 }
